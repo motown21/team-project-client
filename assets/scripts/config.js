b82e3e4e0c000b40f36cee72e87aa198a9a4fa62
motown21 <monique.townson@gmail.com>
2021-01-12 14:24:02 -0800

    Deploy

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://calm-island-25666.herokuapp.com/',
+  production: 'https://calm-island-25666.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
