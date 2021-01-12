26e2f0c207d15d8845cae711b4c4e057d785bdd6
motown21 <monique.townson@gmail.com>
2021-01-12 13:40:52 -0800

    Grunt Deploy

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -2,7 +2,7 @@ const store = require('./../store')
 
 const signUpSuccess = function (response) {
   // creating a message that displays to the user they have sucessfully signed up
-  $('#message').text('Signed Up Successfully!')
+  $('#message-sign-up').text('Signed Up Successfully!')
   $('form').trigger('reset')
 }
 
